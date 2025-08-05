4bf773145a1bb8d9e68f123e436dfd8e8a5c1ee3
Sunny Vuyyuri <addrosity.yt@gmail.com>
2025-08-04 22:44:48 -0400

    Update CodeManager.java

--- DebtCalculator/CodeManager.java
+++ DebtCalculator/CodeManager.java
@@ -1,13 +1,24 @@
 public class CodeManager {
     private String code;
-    private String minimum;
+    private String minimumMonth;
+    private int minimum;
     private int month;
     
     public CodeManager(String c) {
         code = c;
         
-        if(code.equals("A0001")) minimum = "March 2025";
-        if(code.equals("L0001")) minimum = "September 2023";
+        if(code.equals("A0001")) {
+            minimumMonth = "March 2025";
+            minimum = 2;
+        }
+        if(code.equals("L0001")) {
+            minimumMonth = "September 2023";
+            minimum = 21;
+        }
+        if(code.equals("N0001")) {
+            minimumMonth = "August 2025";
+            minimum = 0;
+        }
     }
     
     public CodeManager(int m, String c) {
@@ -15,7 +26,11 @@ public class CodeManager {
         code = c;
     }
     
-    public String getMinimum() {
+    public String getMinimumMonth() {
+        return minimumMonth;
+    }
+    
+    public int getMinimum() {
         return minimum;
     }
     
@@ -29,6 +44,10 @@ public class CodeManager {
             double debt1 = 19.75 * Math.pow(1.07, month);
             double debt2 = 4 * Math.pow(1.07, month - 22);
             return (debt1 + debt2);
+        }
+        if(code.equals("N0001")) {
+            double debt1 = 12.07 * Math.pow(1.07, month);
+            return debt1;
         }
         return -1;
     }
